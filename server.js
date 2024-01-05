fee06b22d6c515c08f839baca5a8308924980dd3
raymondyapkt <134203379+raymondyapkt@users.noreply.github.com>
2024-01-05 09:49:25 +0800

    231201

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
